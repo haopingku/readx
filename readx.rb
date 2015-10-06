06ba188057b0837c5158ba9ced3485da08b934f7
haopingku <haopingku@gmail.com>
2015-10-06 23:21:19 +0800

    Fix load path bugs and add test/elf32

--- readx.rb
+++ readx.rb
@@ -15,21 +15,19 @@ module ReadX
       puts('usage: readx <file> [opt] ...')
     else
       if !File.file?(arg)
-        raise Errno::ENOENT.new(arg)
-      end
-      case File.open(arg,'rb'){|f| f.read(16)}
-      when /^\x7fELF/
-        require_relative 'lib/objdump'
-        Objdump.new(arg).data_js('readx.js')
-        d = File.dirname(__FILE__)
-        s = File.open("#{d}/readx.html", 'r'){|f| f.read}
-                .gsub(/(href|src)="\.\//, "\\1=\"#{Dir.pwd}/#{d}/")
-                /< href="./
-        File.open('readx.html', 'w'){|f| f.puts(s)}
+        puts("readx: file \"#{arg}\" not exist.")
+      else
+        case File.open(arg,'rb'){|f| f.read(16)}
+        when /^\x7fELF/
+          require_relative 'lib/objdump'
+          Objdump.new(arg).data_js('readx.js')
+          d = File.dirname(__FILE__)
+          s = File.open("#{d}/readx.html", 'r'){|f| f.read}
+                  .gsub(/(href|src)="\.\//, "\\1=\"#{d}/")
+          File.open('readx.html', 'w'){|f| f.puts(s)}
+        end
       end
     end
-  # rescue Errno::ENOENT => e
-    # puts("readx: file \"#{arg}\" not exist.")
   rescue NotSupportError => e
     puts(
       "readx: not support \"#{e.not_support}\", " +
